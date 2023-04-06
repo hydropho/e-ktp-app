931771b59911884127798574ce789d268f6b617e
unknown <v-aswin.adnan@gtech.digial>
2023-04-06 09:59:43 +0700

    Update sekrip and .gitlab-ci.yml

--- vue/src/http-common.js
+++ vue/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'http://192.168.78.251:8090/api',
     headers: {
         "Content-type": "application/json",
     },
