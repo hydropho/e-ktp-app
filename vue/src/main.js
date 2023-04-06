d3dee92c962982e5ce0e1ab54013536b9a63d011
Aswin Khairu Adnan <aswinkhad@gmail.com>
2023-04-06 09:39:31 +0700

    Update vue

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,20 +1,22 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import { createWebHistory, createRouter } from 'vue-router'
-// import VueSweetalert2 from 'vue-sweetalert2'
+import VueRouter from 'vue-router'
+import VueSweetalert2 from 'vue-sweetalert2'
 import Routes from './routes.js'
 
-// Vue.use(VueSweetalert2)
-// Vue.config.productionTip = false
-// Vue.use(VueRouter)
+Vue.config.productionTip = false
+Vue.use(VueSweetalert2)
+Vue.use(VueRouter)
 
-const router = createRouter({
-    routes: Routes,
-    history: createWebHistory(),
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history',
 });
 
-router.replace({ redirect: 'auth' })
-// router.push({ path: '/auth' })
+router.replace({ path: '/auth' })
 
-
-createApp(App).use(router).mount('#app')
+new Vue({
+  render: h => h(App),
+  router: router,
+}).$mount('#app')
